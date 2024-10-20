8974e2c0e3084d948a37bc1c502b1439217f02f3
Colum Kelly <columk1@users.noreply.github.com>
2024-10-20 14:50:22 -0700

    feat: init drizzle sqlite schema and lucia session flow

---
+++ src/libs/Lucia.ts
@@ -0,0 +1,80 @@
+import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle'
+import { Lucia, type Session, TimeSpan, type User } from 'lucia'
+import { cookies } from 'next/headers'
+import React from 'react'
+
+import { db } from '@/libs/DB'
+import { sessionTable, userTable } from '@/models/Schema'
+
+const IS_DEV = process.env.NODE_ENV === 'development' ? 'DEV' : 'PROD'
+
+const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable)
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    name: 'user_session',
+    expires: false,
+    attributes: {
+      secure: !IS_DEV,
+    },
+  },
+  sessionExpiresIn: new TimeSpan(1, 'm'), // 1 month
+  // Expose user attributes to the session cookie
+  getUserAttributes: (attributes) => {
+    return {
+      emailVerified: attributes.emailVerified,
+      email: attributes.email,
+    }
+  },
+})
+
+const uncachedValidateRequest = async (): Promise<
+  { user: User, session: Session } | { user: null, session: null }
+> => {
+  const cookieStore = await cookies()
+  const sessionId = cookieStore.get(lucia.sessionCookieName)?.value ?? null
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    }
+  }
+
+  const result = await lucia.validateSession(sessionId)
+  // next.js throws when you attempt to set cookie when rendering page
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id)
+      const cookieStore = await cookies()
+      cookieStore.set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      )
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie()
+      const cookieStore = await cookies()
+      cookieStore.set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      )
+    }
+  } catch {}
+  return result
+}
+
+export const validateRequest = React.cache(uncachedValidateRequest)
+
+declare module 'lucia' {
+  // Extend Register type to include user fields
+  // eslint-disable-next-line ts/consistent-type-definitions
+  interface Register {
+    Lucia: typeof lucia
+    DatabaseUserAttributes: {
+      email: string
+      emailVerified: number
+    }
+  }
+}
--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -1,5 +1,6 @@
-import { sql } from 'drizzle-orm'
-import { integer, sqliteTable } from 'drizzle-orm/sqlite-core'
+import { relations, sql } from 'drizzle-orm'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { ulid } from 'ulidx'
 
 // To modify the database schema:
 // 1. Update this file with your desired changes.
@@ -14,3 +15,64 @@ export const counterSchema = sqliteTable('counter', {
   createdAt: integer('created_at').default(sql`(cast(unixepoch() as int))`),
   updatedAt: integer('updated_at').default(sql`(cast(unixepoch() as int))`),
 })
+
+export const userTable = sqliteTable('user', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => ulid()),
+  email: text('email').unique().notNull(),
+  password: text('password').notNull(),
+  name: text('name').notNull(),
+  username: text('username').notNull(),
+  emailVerified: integer('email_verified').notNull(),
+})
+
+export const sessionTable = sqliteTable('session', {
+  id: text('id').primaryKey(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => userTable.id, {
+      onUpdate: 'cascade',
+      onDelete: 'cascade',
+    }),
+  expiresAt: integer('expires_at').notNull(),
+})
+
+export const emailVerificationCodeTable = sqliteTable(
+  'email_verification_code',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => ulid()),
+    code: text('code'),
+    userId: text('user_id')
+      .notNull()
+      .references(() => userTable.id, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      }),
+    expiresAt: integer('expires_at'),
+  },
+)
+
+export const userTableRelations = relations(userTable, ({ many }) => ({
+  session: many(sessionTable),
+  emailVerificationCode: many(emailVerificationCodeTable),
+}))
+
+export const sessionTableRelations = relations(sessionTable, ({ one }) => ({
+  user: one(userTable, {
+    fields: [sessionTable.userId],
+    references: [userTable.id],
+  }),
+}))
+
+export const emailVerificationCodeRelations = relations(
+  emailVerificationCodeTable,
+  ({ one }) => ({
+    user: one(userTable, {
+      fields: [emailVerificationCodeTable.userId],
+      references: [userTable.id],
+    }),
+  }),
+)
