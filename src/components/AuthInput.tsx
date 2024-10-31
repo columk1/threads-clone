cb01576f2c444c585448b1381bbffb965f93d0e6
Colum Kelly <columk1@users.noreply.github.com>
2024-10-31 00:57:02 -0700

    feat: add toggle to show password

--- src/components/AuthInput.tsx
+++ src/components/AuthInput.tsx
@@ -1,5 +1,5 @@
 import cx from 'clsx'
-import { CircleCheck, CircleX } from 'lucide-react'
+import { CircleCheck, CircleX, Eye, EyeOff } from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
 
 import { createErrorMessageLookup, debounce, getError } from '@/utils/Helpers'
@@ -41,6 +41,7 @@ const Input = ({
 }: AuthInputProps) => {
   const [validationMessage, setValidationMessage] = useState<string | null>(error)
   const [activeError, setActiveError] = useState(false)
+  const [passwordVisible, setPasswordVisible] = useState(false)
   const inputRef = useRef<HTMLInputElement>(null)
 
   const errorMessages = createErrorMessageLookup(name)
@@ -78,19 +79,28 @@ const Input = ({
     validate(e.target, customValidator)
   }
 
+  const togglePasswordVisiblity = () => {
+    setPasswordVisible(!passwordVisible)
+  }
+
+  const isPassword = type === 'password'
+
+  const inputType = isPassword ? (passwordVisible ? 'text' : 'password') : type
+
   return (
     <div className="relative">
       <input
         ref={inputRef}
-        type={type}
+        type={inputType}
         name={name}
         placeholder={placeholder}
         className={cx(
-          'text-input p-4 peer pr-10 h-12 w-full rounded-xl border border-transparent bg-tertiary-bg font-sans font-light placeholder:text-placeholder-text focus:border focus:border-primary-outline focus:outline-0',
+          'text-input p-4 peer h-12 w-full rounded-xl border border-transparent bg-tertiary-bg font-sans font-light placeholder:text-placeholder-text focus:border focus:border-primary-outline focus:outline-0',
           !placeholder && '[&:not(:placeholder-shown)]:pb-0',
           `${className}`,
           activeError && '[&:user-invalid]:border-red-500',
           error && 'peer-invalid:border-red-500',
+          isPassword ? 'pr-[72px]' : 'pr-10',
         )}
         id={name}
         defaultValue={defaultValue}
@@ -107,10 +117,11 @@ const Input = ({
       </label>
       {icons && (
         <>
-          {activeError && <CircleX className={`absolute right-2 top-[14px] hidden text-red-500 ${error && 'peer-invalid:block'} peer-[&:user-invalid]:block`} />}
-          {activeError && !validationMessage && <CircleCheck className="absolute right-2 top-[14px] hidden text-placeholder-text peer-[&:not(:focus-within):user-valid]:block" />}
+          {activeError && <CircleX className={`absolute ${isPassword ? 'right-[40px]' : 'right-2'} top-[14px] hidden text-red-500 ${error && 'peer-invalid:block'} peer-[&:user-invalid]:block`} />}
+          {activeError && !validationMessage && <CircleCheck className={`absolute ${isPassword ? 'right-10' : 'right-2'} right-2 top-[14px] hidden text-placeholder-text peer-[&:not(:focus-within):user-valid]:block`} />}
         </>
       )}
+      {isPassword && <button type="button" onClick={togglePasswordVisiblity} className="absolute right-2 top-[14px] hidden text-gray-7 peer-[&:not(:placeholder-shown)]:block">{passwordVisible ? <EyeOff className="w-6" /> : <Eye />}</button>}
       {activeError && <p className={`hidden py-1 pl-4 text-sm text-red-500 ${error && 'peer-invalid:block'} peer-[&:user-invalid]:block`}>{validationMessage || error}</p>}
     </div>
   )
