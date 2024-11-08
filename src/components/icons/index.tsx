e5dfd72e4a08f7251df9ec96ea6a2c09d5ac94c4
Colum Kelly <columk1@users.noreply.github.com>
2024-11-08 01:19:42 -0800

    feat: add call-to-action icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -3,11 +3,15 @@ import CheckmarkIcon from './Checkmark'
 import CreateIcon from './Create'
 import EditIcon from './Edit'
 import HomeIcon from './Home'
+import Like from './Like'
 import MoreIcon from './More'
 import NotificationsIcon from './Notifications'
 import NotificationsFooterIcon from './NotificationsFooter'
 import PinIcon from './Pin'
 import ProfileIcon from './Profile'
+import Reply from './Reply'
+import Repost from './Repost'
 import SearchIcon from './Search'
+import Share from './Share'
 
-export { CaretIcon, CheckmarkIcon, CreateIcon, EditIcon, HomeIcon, MoreIcon, NotificationsFooterIcon, NotificationsIcon, PinIcon, ProfileIcon, SearchIcon }
+export { CaretIcon, CheckmarkIcon, CreateIcon, EditIcon, HomeIcon, Like, MoreIcon, NotificationsFooterIcon, NotificationsIcon, PinIcon, ProfileIcon, Reply, Repost, SearchIcon, Share }
