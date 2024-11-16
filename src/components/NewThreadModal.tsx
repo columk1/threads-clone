d7d6bfc2631493da6d0c64b9e1e28c6dabdf51d0
Colum Kelly <columk1@users.noreply.github.com>
2024-11-16 00:42:53 -0800

    feat: responsive new thread dialog, drawer and modal

--- src/components/NewThreadModal.tsx
+++ src/components/NewThreadModal.tsx
@@ -5,8 +5,10 @@ import { toast } from 'sonner'
 
 import { createPost } from '@/app/actions'
 import { ModalContext } from '@/context/ModalContext'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from './Dialog'
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle } from './Dialog'
+import { Drawer, DrawerContent } from './Drawer'
 
 type NewThreadModalProps = {
   user?: any
@@ -38,15 +40,64 @@ const NewThreadModal: FunctionComponent<NewThreadModalProps> = () => {
     }
   }, [state, router])
 
+  const isDesktop = useMediaQuery('(min-width: 700px)')
+
+  if (isDesktop) {
+    return (
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="min-w-[620px] max-md:hidden">
+          <div className="sr-only">
+            <DialogDescription>Create a new thread</DialogDescription>
+          </div>
+          <DialogHeader className="grid h-14 grid-cols-[minmax(64px,100px)_minmax(0,1fr)_minmax(64px,100px)]">
+            <DialogClose asChild>
+              <div className="flex items-center justify-center">
+                <button type="button" onClick={closeModal} className="rounded-lg py-1 text-[17px]">
+                  <span className="sr-only">Close</span>
+                  Cancel
+                </button>
+              </div>
+            </DialogClose>
+            <DialogTitle className="col-start-2 place-self-center text-[16px] font-bold">
+              New thread
+            </DialogTitle>
+          </DialogHeader>
+          <form action={formAction}>
+            <div className="grid grid-cols-[48px_minmax(0,1fr)] gap-y-[3px] text-[15px]">
+              <div className="col-start-1 row-start-1 row-end-[span_2] pt-1 ">
+                <div className="size-9 rounded-full bg-gray-7"></div>
+              </div>
+              <div className="flex w-full items-center justify-between gap-2">
+                <div className="flex items-center gap-2">
+                  <div className="font-semibold">
+                    {/* { row.user.username } */}
+                    columk1
+                  </div>
+                </div>
+              </div>
+              {/* eslint-disable-next-line jsx-a11y/no-autofocus */}
+              <input type="text" autoComplete="off" autoFocus onChange={handleInput} name="text" placeholder="What's new?" minLength={1} className="col-start-2 mb-[2px] bg-transparent placeholder:text-gray-7 focus:outline-none focus:ring-0" />
+            </div>
+            <div className="flex items-center justify-between pt-6 text-[15px] text-gray-7">
+              Anyone can reply & quote
+              <button type="submit" onClick={closeModal} disabled={!isValid || isPending} className="ml-auto h-9 rounded-lg border border-gray-5 px-4 font-semibold text-primary-text transition active:scale-95 disabled:opacity-30">
+                Post
+              </button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+    )
+  }
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="min-w-[620px] max-md:hidden">
+    <Drawer open={isOpen} onOpenChange={setIsOpen}>
+      <DrawerContent className="h-full min-w-full border-none">
         <div className="sr-only">
           <DialogDescription>Create a new thread</DialogDescription>
         </div>
         <DialogHeader className="grid h-14 grid-cols-[minmax(64px,100px)_minmax(0,1fr)_minmax(64px,100px)]">
           <DialogClose asChild>
-            <div className="flex items-center justify-center">
+            <div className="flex">
               <button type="button" onClick={closeModal} className="rounded-lg py-1 text-[17px]">
                 <span className="sr-only">Close</span>
                 Cancel
@@ -57,8 +108,8 @@ const NewThreadModal: FunctionComponent<NewThreadModalProps> = () => {
             New thread
           </DialogTitle>
         </DialogHeader>
-        <form action={formAction}>
-          <div className="grid grid-cols-[48px_minmax(0,1fr)] gap-y-[3px] text-[15px]">
+        <form action={formAction} className="flex h-[calc(100%-56px)] w-full flex-col justify-between">
+          <div className="grid grid-cols-[48px_minmax(0,1fr)] gap-y-[3px] pt-3 text-[15px]">
             <div className="col-start-1 row-start-1 row-end-[span_2] pt-1 ">
               <div className="size-9 rounded-full bg-gray-7"></div>
             </div>
@@ -73,17 +124,15 @@ const NewThreadModal: FunctionComponent<NewThreadModalProps> = () => {
             {/* eslint-disable-next-line jsx-a11y/no-autofocus */}
             <input type="text" autoComplete="off" autoFocus onChange={handleInput} name="text" placeholder="What's new?" minLength={1} className="col-start-2 mb-[2px] bg-transparent placeholder:text-gray-7 focus:outline-none focus:ring-0" />
           </div>
-          <DialogFooter>
-            <div className="flex items-center justify-between pt-6 text-[15px] text-gray-7">
-              Anyone can reply & quote
-              <button type="submit" onClick={closeModal} disabled={!isValid || isPending} className="ml-auto h-9 rounded-lg border border-gray-5 px-4 font-semibold text-primary-text transition active:scale-95 disabled:opacity-30">
-                Post
-              </button>
-            </div>
-          </DialogFooter>
+          <div className="mx-auto flex items-center justify-between gap-4 text-[15px] text-gray-7">
+            Anyone can reply & quote
+            <button type="submit" onClick={closeModal} disabled={!isValid || isPending} className="ml-auto h-9 rounded-2xl bg-primary-text px-4 font-semibold text-gray-0 transition active:scale-95 disabled:opacity-30">
+              Post
+            </button>
+          </div>
         </form>
-      </DialogContent>
-    </Dialog>
+      </DrawerContent>
+    </Drawer>
   )
 }
 
