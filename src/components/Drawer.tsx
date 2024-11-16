2dd4e4f744b245f04585bd5341dcb4dd4b293ff7
Colum Kelly <columk1@users.noreply.github.com>
2024-11-16 00:57:28 -0800

    fix: animation duration class not applied

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -37,7 +37,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'antialiased fixed left-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-100%] gap-2 border-[0.5px] border-gray-200 px-6 pb-4 bg-white shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:fade-out-0 data-[state=open]:fade-in-100 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[100%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[0%] rounded-2xl dark:border-gray-5 dark:bg-gray-2',
+        'antialiased fixed left-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-100%] gap-2 border-[0.5px] border-gray-200 px-6 pb-4 bg-white shadow-lg data-[state=open]:animate-in !duration-200 data-[state=closed]:fade-out-0 data-[state=open]:fade-in-100 data-[state=closed]:slide-out-to-top-[100%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[0%] rounded-2xl dark:border-gray-5 dark:bg-gray-2',
         className,
       )}
       {...props}
