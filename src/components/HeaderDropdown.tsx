7966b4814007bbfa3227555b499530358388b5f1
Colum Kelly <columk1@users.noreply.github.com>
2024-11-19 12:09:25 -0800

    fix: dropdown items should use asChild prop

--- src/components/HeaderDropdown.tsx
+++ src/components/HeaderDropdown.tsx
@@ -23,38 +23,38 @@ export const HeaderDropdown: FunctionComponent<HeaderDropdownProps> = ({ pathnam
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent alignOffset={0} sideOffset={6} className="hidden w-60 origin-top text-[15px] md:block">
-        <Link href="/">
-          <DropdownMenuItem className="py-3.5">
+        <DropdownMenuItem asChild className="py-3.5">
+          <Link href="/">
             <div className="flex flex-1 items-center justify-between">
               <span>For you</span>
               {pathname === '/' && <CheckmarkIcon />}
             </div>
-          </DropdownMenuItem>
-        </Link>
-        <Link href="/following">
-          <DropdownMenuItem className="py-3.5">
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild className="py-3.5">
+          <Link href="/following">
             <div className="flex flex-1 items-center justify-between">
               <span>Following</span>
               {pathname === '/following' && <CheckmarkIcon />}
             </div>
-          </DropdownMenuItem>
-        </Link>
-        <Link href="/liked">
-          <DropdownMenuItem className="py-3.5">
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild className="py-3.5">
+          <Link href="/liked">
             <div className="flex flex-1 items-center justify-between">
               <span>Liked</span>
               {pathname === '/liked' && <CheckmarkIcon />}
             </div>
-          </DropdownMenuItem>
-        </Link>
-        <Link href="/saved">
-          <DropdownMenuItem className="py-3.5">
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild className="py-3.5">
+          <Link href="/saved">
             <div className="flex flex-1 items-center justify-between">
               <span>Saved</span>
               {pathname === '/saved' && <CheckmarkIcon />}
             </div>
-          </DropdownMenuItem>
-        </Link>
+          </Link>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
