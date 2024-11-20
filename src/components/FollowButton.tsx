79630b5dd56a3494428d2258754dcffdcd937c19
Colum Kelly <columk1@users.noreply.github.com>
2024-11-20 00:00:00 -0800

    fix: follow button replace useoptimistic hook

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import cx from 'clsx'
-import { type FunctionComponent, startTransition, useOptimistic } from 'react'
+import type { FunctionComponent } from 'react'
 
 type FollowButtonProps = {
   isFollowed: boolean
@@ -9,28 +9,16 @@ type FollowButtonProps = {
 }
 
 const FollowButton: FunctionComponent<FollowButtonProps> = ({ isFollowed, onToggleFollow }) => {
-  const [optimisticIsFollowed, addOptimisticIsFollowed] = useOptimistic(
-    isFollowed,
-    (_, optimisticValue: boolean) => optimisticValue,
-  )
-
-  const handleToggleFollow = async () => {
-    startTransition(() => {
-      addOptimisticIsFollowed(!optimisticIsFollowed)
-    })
-    await onToggleFollow()
-  }
-
   return (
     <button
       type="button"
-      onClick={handleToggleFollow}
+      onClick={onToggleFollow}
       className={cx(
         'w-full h-9 rounded-lg border border-gray-5 px-4 text-[15px] font-semibold transition active:scale-95 disabled:opacity-30',
-        !optimisticIsFollowed ? 'bg-white text-black' : 'text-primary-text',
+        !isFollowed ? 'bg-white text-black' : 'text-primary-text',
       )}
     >
-      {optimisticIsFollowed ? 'Unfollow' : 'Follow'}
+      {isFollowed ? 'Unfollow' : 'Follow'}
     </button>
   )
 }
