cd13e7dc2fb10d2e5b0d9d18f4c5d89f05f00908
Colum Kelly <columk1@users.noreply.github.com>
2025-01-02 22:44:28 -0800

    fix(deps): update drizzle to allow check constraints

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   out: './migrations',
   schema: './src/models/Schema.ts',
-  dialect: 'sqlite',
-  driver: 'turso',
+  dialect: 'turso',
   dbCredentials: {
     url: process.env.DATABASE_URL ?? '',
     authToken: process.env.TURSO_AUTH_TOKEN ?? '',
