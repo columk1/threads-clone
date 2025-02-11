cd76d8a7c45a02ea685453989fa9e0a9224a30d8
Colum Kelly <columk1@users.noreply.github.com>
2025-02-10 23:17:14 -0800

    chore: add media query to FAB

--- src/components/NewPostFAB.tsx
+++ src/components/NewPostFAB.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import type { FunctionComponent } from 'react'
+
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 
 import { CreateIcon } from './icons'
 import PostButton from './PostButton'
 
 const NewPostFAB: FunctionComponent = () => {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+  if (!isDesktop) {
+    return null
+  }
+
   return (
     <div className="fixed bottom-6 right-6 flex h-[67px] w-[81px] items-center justify-center delay-200 duration-300 animate-in fade-in-0 zoom-in-75">
       <PostButton className="size-full">
